ce066d33a8a27176845eab3b50006054de8855b7
Louay-Ben-nessir <91753931+Louay-Ben-nessir@users.noreply.github.com>
2021-10-01 19:35:59 +0100

    Delete pices.py

--- pices.py
+++
@@ -1,9 +0,0 @@
-class Pice:
-    def __init__(self,postion,colour,Pice_type):
-        self.possible_moves=[]
-        self.postion=postion
-        self.colour=colour
-        self.type=Pice_type
-    def check_possible_moves(self,board):
-        pass
-    
